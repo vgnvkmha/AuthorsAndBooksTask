1422273c187554e689bda61ec286a2b3aae59020
vgnvkmha <pppavlov2019@gmail.com>
2025-04-12 17:31:23 +0300

    "Login" now with brush + Color.kt file was created to store colors values

---
+++ app/src/main/java/com/example/authorsandbookstask/view/ui/Color.kt
@@ -0,0 +1,8 @@
+package com.example.authorsandbookstask.view.ui
+
+
+import androidx.compose.ui.graphics.Color
+
+
+val myGreen = Color(0xFF0EDA2E)
+val myPurple = Color(0xFF9216FF)
--- app/src/main/java/com/example/authorsandbookstask/view/ui/LoginActivity.kt
+++ app/src/main/java/com/example/authorsandbookstask/view/ui/LoginActivity.kt
@@ -3,12 +3,9 @@ package com.example.authorsandbookstask.view.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,25 +13,40 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 class LoginActivity : ComponentActivity() {
     private var testString = "test"
+    private val colors = listOf(Color.Cyan, Color.Blue, myPurple)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        lifecycleScope.launch {
 //            allUsers = KtorClient.client.get("http://10.0.2.2:8080/all_users").body()
 //        }
         setContent {
-            Column {
+            Column(modifier = Modifier.fillMaxWidth()
+                .padding(16.dp
+            )) {
+                Text(text = "Login",
+                    fontSize = 40.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        brush = Brush.linearGradient(
+                            colors = colors
+                        )
+                    )
+                )
                 CustomLoginWindow("username",
                     "text", onTextChange = {testString = it}
                 )
