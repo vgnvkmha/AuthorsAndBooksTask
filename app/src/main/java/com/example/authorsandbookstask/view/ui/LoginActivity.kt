d5aefa78b160e00887379033f65a5bec0affcc39
vgnvkmha <pppavlov2019@gmail.com>
2025-04-12 16:57:53 +0300

    Composable for LoginActivity was created

--- app/src/main/java/com/example/authorsandbookstask/view/ui/LoginActivity.kt
+++ app/src/main/java/com/example/authorsandbookstask/view/ui/LoginActivity.kt
@@ -2,13 +2,82 @@ package com.example.authorsandbookstask.view.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 class LoginActivity : ComponentActivity() {
+    private var testString = "test"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        lifecycleScope.launch {
 //            allUsers = KtorClient.client.get("http://10.0.2.2:8080/all_users").body()
 //        }
+        setContent {
+            Column {
+                CustomLoginWindow("username",
+                    "text", onTextChange = {testString = it}
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                CustomLoginWindow("password",
+                    "text", onTextChange = {testString = it})
+            }
+
+        }
+    }
+}
+
+
+@Composable
+fun CustomLoginWindow(
+    label: String,
+    text: String,
+    onTextChange: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            text = label,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        TextField(
+            value = text,
+            onValueChange = onTextChange,
+            label = { Text("Enter $label") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
